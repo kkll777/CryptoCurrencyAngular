4157e33023bb7959fb33287c476ea57c26141f1a
etanline2103 <etanline2103@rstn.com.tw>
2022-05-18 12:00:06 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularCryptoCurrencyPricePicker';
+  selectCurrency !: string;
+  sendCurrency(event:any) {
+    console.log(event)
+  }
 }
